c3752aa50018e090578e7b4016d5d0e8e1665e6c
Jason <kangja1996@gmail.com>
2021-08-18 16:12:46 -0400

    finished

--- practice.js
+++ practice.js
@@ -26,12 +26,12 @@ this.table = "window table";
 //   innterFunction.bind(this)(soap);
 // };
 
-const cleanTable = function (soap) {
-  const innterFunction = (_soap) => {
-    console.log(`cleaning ${this.table} using ${_soap}`);
-  };
-  innterFunction(soap);
-};
+// const cleanTable = function (soap) {
+//   const innterFunction = (_soap) => {
+//     console.log(`cleaning ${this.table} using ${_soap}`);
+//   };
+//   innterFunction(soap);
+// };
 
 // cleanTable.call(this, "some soap");
 
@@ -46,16 +46,40 @@ this.garage = {
 
 // this.garage.table;
 
-let johnsRoom = {
-  table: "johns table",
-  //   cleanTable() {
-  //     console.log(`cleaning ${this.table}`);
-  //   },
-};
+// let johnsRoom = {
+//   table: "johns table",
+//   //   cleanTable() {
+//   //     console.log(`cleaning ${this.table}`);
+//   //   },
+// };
+
+// let createRoom = function (name) {
+//   this.table = `${name}s table`;
+// };
+
+class createRoom {
+  constructor(name) {
+    this.name = `${name}s table`;
+  }
+  cleanTable(soap) {
+    console.log(`cleaning ${this.table} using ${soap}`);
+  }
+}
+
+// createRoom.prototype.cleanTable = function (soap) {
+//   console.log(`cleaning ${this.table} using ${soap}`);
+// };
+
+const jasonsRoom = new createRoom("Jason");
+const johnsRoom = new createRoom("John");
+
+jasonsRoom.cleanTable("some soap");
+johnsRoom.cleanTable("some soap");
 
 // console.log(johnsRoom.table);
 // johnsRoom.cleanTable();
 // this.garage.cleanTable();
-cleanTable.call(this, "some soap");
-cleanTable.call(this.garage, "some soap");
-cleanTable.call(johnsRoom, "some soap");
+// cleanTable.call(this, "some soap");
+// cleanTable.call(this.garage, "some soap");
+// cleanTable.call(johnsRoom, "some soap");
+// cleanTable.call(jasonsRoom, "some soap");
